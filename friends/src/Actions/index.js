163f4a58e84d0b47e04d1e6be62b3ebe1e6aa188
Brandon Allison <brandona8128@outlook.com>
2019-04-11 10:13:36 -0400

    began work on actions

---
+++ friends/src/Actions/index.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+export const LOGGING_IN = "LOGGING_IN";
+export const LOGIN_SUCCESS = "LOGIN_SUCCESS";
+export const LOGIN_FAILURE = "LOGIN_FAILURE";
+export const FETCH_FRIENDS = "FETCH_FRIENDS";
+export const FETCH_COMPLETE = "FETCH_COMPLETE";
+export const FETCH_FAILED = "FETCH_FAILED";
+
+
---
+++ friends/src/Reducers/rootReducer.js
@@ -0,0 +1,9 @@
+import {
+    LOGGING_IN,
+    LOGIN_SUCCESS,
+    LOGIN_FAILURE,
+    FETCH_FRIENDS,
+    FETCH_GOOD,
+    FETCH_BAD
+} from '../Actions';
+
